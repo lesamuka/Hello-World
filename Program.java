01dfa4ab708214e8c304b8c4bf0310e5663ee0c0
lesamuka <42079758+lesamuka@users.noreply.github.com>
2018-08-19 22:11:56 -0300

    Code Hello world!

---
+++ Program.java
@@ -0,0 +1,18 @@
+package course;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Hello world!");
+		
+		sc.close();
+
+	}
+
+}
